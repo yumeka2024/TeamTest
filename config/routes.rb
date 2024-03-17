9d446ea20edb18d91a910034c170d6541bc60e30
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-03-17 03:05:45 +0000

    [Add]MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  # devise_for :admins
+  # devise_for :customers
+  
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
